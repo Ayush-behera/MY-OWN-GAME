dd11a988624f0f43e9a95a351a1fc7a1a13b52e4
Ayush-behera <73184859+Ayush-behera@users.noreply.github.com>
2021-05-04 19:28:25 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,26 @@
+var circle;
+var letter;
+var gamestate = 'start';
+var score = 0;
+
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(1000,500);
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  fill("red")
+  circle(500,250,80)
+
+
+  drawSprites();
+}
+ 
